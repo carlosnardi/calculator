8c6470edcfce87d494e97870020e5ce3bc895b89
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2024-10-23 20:55:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# It should work like the example above: 
+"""
+What's the first number: 5
++
+-
+*
+/
+Pick an operation: /
+What's the next number: 2
+5 / 2 = 2.5
+Type 'y' to continue calculating with 2.5, or type 'n' to start a new calculation: y
+Pick an operation: /
+What's the next number: 2
+
+"""
