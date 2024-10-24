0f6c852d6cb52e8ee0195a7dd33d2c2e0c81f00b
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2024-10-24 09:58:33 +0000

    Initial code for getting data first

--- main.py
+++ main.py
@@ -13,3 +13,33 @@ Pick an operation: /
 What's the next number: 2
 
 """
+def sum(num1, num2):
+  return num1 + num2
+
+def subs(num1, num2):
+  return num1 - num2
+
+def mult(num1, num2):
+  return num1 * num2
+
+def div(num1, num2):
+  return num1 / num2
+
+def getting_data():
+  num1 = float(input("What's the first number: ")
+  operation = input('''
+  +
+  -
+  *
+  /
+  Pick an operation: ''')
+  num2 = float(input("What's the next number: "))
+
+def continue_calculating(number):
+  operation = input('''
+  +
+  -
+  *
+  /
+  Pick an operation: ''')
+  num2 = float(input("What's the next number: "))
