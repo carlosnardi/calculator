791f1edfbaef174bfdb192a5ad2c4631d368112c
Carlos Nardi <37674568+carlosnardi@users.noreply.github.com>
2024-10-24 15:56:28 +0000

    Calculator program nearing completion.

--- calc.py
+++ calc.py
@@ -1,18 +1,3 @@
-# It should work like the example above: 
-"""
-What's the first number: 5
-+
--
-*
-/
-Pick an operation: /
-What's the next number: 2
-5 / 2 = 2.5
-Type 'y' to continue calculating with 2.5, or type 'n' to start a new calculation: y
-Pick an operation: /
-What's the next number: 2
-
-"""
 def getting_data():
   num1 = float(input("What's the first number: "))
   operation = input('''
@@ -24,7 +9,7 @@ def getting_data():
   num2 = float(input("What's the next number: "))
   return num1, num2, operation
 
-def continue_calculating(number):
+def continue_calculating():
   operation = input('''
   +
   -
@@ -43,16 +28,20 @@ def calculating(num1, num2, operation):
     return num1 * num2
   elif operation == "/": 
     return num1 / num2
-    
 
-data = getting_data()
-num1 = data[0]
-num2 = data[1]
-operation = data[2]
-
-first_result = calculating(num1, num2, operation)
-print(first_result)
-
-#continue_calc = input(f"Type 'y' to continue calculating with {first_result}, or type 'n' to start a new calculation:\n")
-#if continue_calc == "y":
- # continue_calculating(first_result)
+while True:
+  data = getting_data()
+  num1 = data[0]
+  num2 = data[1]
+  operation = data[2]
+  first_result = calculating(num1, num2, operation)
+  print(first_result)
+  
+  continue_calc = input(f"Type 'y' to continue calculating with {first_result}, or type 'n' to start a new calculation:\n")
+  while continue_calc == "y":
+    data_to_continue = continue_calculating()
+    num2 = data_to_continue[0]
+    operation = data_to_continue[1]
+    second_result = calculating(first_result, num2, operation)
+    print(second_result)
+    continue_calc = input(f"Type 'y' to continue calculating with {first_result}, or type 'n' to start a new calculation:\n")
